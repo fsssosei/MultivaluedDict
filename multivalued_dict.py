a072d29bc2660059212d09277b1fde917c84eb9c
fsssosei <43291778+fsssosei@users.noreply.github.com>
2019-06-06 06:25:01 +0800

    Update multivalued_dict.py

--- multivalued_dict.py
+++ multivalued_dict.py
@@ -7,21 +7,21 @@ from itertools import filterfalse
 
 class MultivaluedDictError(Exception):
     pass
-    
+
 class KeyValuePairsError(MultivaluedDictError):
     def __init__(self, list_of_not_kv_pair):
         self.list_of_not_kv_pair = list_of_not_kv_pair
-        
+
     def __repr__(self):
         return f'{list_of_not_kv_pair} these items do not form key-value pairs. '
 
 class HashError(MultivaluedDictError):
     def __init__(self, list_of_not_hash):
         self.list_of_not_hash = list_of_not_hash
-        
+
     def __repr__(self):
         return f'{list_of_not_hash} these are unhashable. '
-    
+
 class multivalued_dict(UserDict):
     START = 'S'
     END = 'E'
@@ -29,7 +29,7 @@ class multivalued_dict(UserDict):
     @classmethod
     def __is_multivalued_dict__(cls, x):
         return (isinstance(x, multivalued_dict) or ((True if x.default_factory == type([]) else False) if isinstance(x, defaultdict) else False))
-    
+
     def __init__(self, initial_items):
         self.data = defaultdict(list)
         if multivalued_dict.__is_multivalued_dict__(initial_items):
@@ -42,23 +42,23 @@ class multivalued_dict(UserDict):
             self.data = multivalued_init_items
         else:
             raise TypeError(f"{type(multivalued_init_items)}  objects are not multivalued_dict or defaultdict(<class 'list'>) objects. ")
-    
+
     def __repr__(self):
         return f'multivalued_dict({dict(self.data)})'
-    
+
     def __lenvalue__(self, key = None):
         if key == None:
             return sum(map(len, self.data.values()))
         else:
             return len(self.data[key])
-    
+
     def __matchkv__(self, key, value):
         return value in self.data[key]
-        
+
     def __delkv__(self, key, value, allkv = True, direction = START):
         assert allkv in (True, False), '"allkv" can only be True or False.'
         assert direction in (self.START, self.END), '"direction" can only be START or END.'
-        
+
         if allkv:
             while value in self.data[key]:
                 self.data[key].remove(value)
@@ -74,7 +74,7 @@ class multivalued_dict(UserDict):
 
     def count(self, key, value):
         return self.data[key].count(value)
-    
+
     def update(self, update_items):
         if isinstance(update_items, Iterable):
             if isinstance(update_items, dict):
@@ -93,22 +93,22 @@ class multivalued_dict(UserDict):
                     raise KeyValuePairsError(list_of_not_kv_pair)
         else:
             raise TypeError(f'{type(update_items)} object is not iterable. ')
-    
+
     def reverse(self, key):
         self.data[key].reverse()
-    
+
     def copy(self):
         return multivalued_dict(self.data)
-    
+
     def items(self):
         return self.data.items()
-    
+
     def keys(self):
         return self.data.keys()
-    
+
     def values(self):
         return self.data.values()
-    
+
     @classmethod
     def fromkeys(cls, initial_keys, value = None):
         dict_var = dict.fromkeys(initial_keys, value)
