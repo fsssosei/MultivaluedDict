3a245da11a563cc54fdfbaaca885b0b44c0a8d4d
fsssosei <43291778+fsssosei@users.noreply.github.com>
2019-07-29 05:47:14 +0800

    Update doctestmod_module.py

--- src/multivalued_dict_package/doctestmod_module.py
+++ src/multivalued_dict_package/doctestmod_module.py
@@ -1,7 +1,6 @@
-from doctest import testmod
-
 __all__ = ['doctestmod']
 
 def doctestmod():
+    from doctest import testmod
     from multivalued_dict_package import multivalued_dict_module
     testmod(multivalued_dict_module)
