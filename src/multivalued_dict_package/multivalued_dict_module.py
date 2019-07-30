af7f5bd1c7b5bb832fc81dab4e2dfc22741a9120
fsssosei <43291778+fsssosei@users.noreply.github.com>
2019-07-31 02:34:22 +0800

    Update multivalued_dict_module.py

--- src/multivalued_dict_package/multivalued_dict_module.py
+++ src/multivalued_dict_package/multivalued_dict_module.py
@@ -27,10 +27,10 @@ __all__ = ['multivalued_dict', 'START_POS', 'END_POS']
 START_POS = 'S'
 END_POS = 'E'
 
-class __eliminate_metaclass_conflicts(check_self_class_call_of_meta, ABCMeta):  #lgtm [py/missing-call-to-init]
+class __eliminate_metaclass_conflicts(check_self_class_call_of_meta, ABCMeta):
     pass
 
-class multivalued_dict(UserDict, metaclass = __eliminate_metaclass_conflicts):
+class multivalued_dict(UserDict, metaclass = __eliminate_metaclass_conflicts):  #lgtm [py/missing-call-to-init]
     '''
         multivalued_dict() -> new empty dictionary
         multivalued_dict(mapping) -> new dictionary initialized from a mapping object's
